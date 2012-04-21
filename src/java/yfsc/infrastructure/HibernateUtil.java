7d638e993ab9d7345ef9ca70b379c747c33bd87c
Kirk Thompson <kwt104@gmail.com>
2012-04-21 09:31:50 -0400

    upgrade hibernate to version 4... this was a pain in the ass

--- src/java/yfsc/infrastructure/HibernateUtil.java
+++ src/java/yfsc/infrastructure/HibernateUtil.java
@@ -1,14 +1,21 @@
 package yfsc.infrastructure;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory;
-    static {
+    
+	private static final SessionFactory sessionFactory;
+    private static final ServiceRegistry serviceRegistry;
+	
+	static {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+			Configuration configuration = new Configuration().configure();
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         } 
         catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
